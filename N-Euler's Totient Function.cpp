fc6fc53da077efeec1c339cc54e4a57d6b2ceaa0
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-03-14 11:41:55 +0600

    Update N-Euler's Totient Function.cpp

--- N-Euler's Totient Function.cpp
+++ N-Euler's Totient Function.cpp
@@ -47,3 +47,55 @@ int main()
 9
 10
 */
+//In a range
+
+#include<bits/stdc++.h>
+#define MAX 1000000
+
+using namespace std;
+
+int arr[MAX];
+
+void phi(int n)
+{
+    int i, j;
+    for(i = 1; i <= n; i++)
+        arr[i] = i;
+
+    for(i = 2; i <= n; i++){
+        if(arr[i] == i){
+            for(j = i; j <= n; j += i){
+                arr[j] = (arr[j] * (i-1)) / i;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    int n, i, t, j;
+    cin>> t;
+    phi(100000);
+
+    for(i = 1; i <= t; i++){
+        cin>> n;
+        cout<< arr[n] << "\n";
+    }
+
+
+    return 0;
+}
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
+
