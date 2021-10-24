624673015d2a8bf43cf102919a68c6c90ede32ba
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-10-24 18:05:40 +0600

    Create binary_search.cpp

---
+++ Searching/binary_search.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool binarySearch(int arr[], int n, int k);
+
+int main()
+{
+    int n, i, k, arr[100001];
+    cin>> n >> k;
+
+    for (i = 0; i < n; i++)
+        cin>> arr[i];
+
+    cout<< binarySearch(arr, n, k) << "\n";
+
+    return 0;
+}
+
+
+bool binarySearch(int arr[], int n, int k)
+{
+    int low, high, mid;
+    low = 0;
+    high = n-1;
+
+    while (low <= high)
+    {
+        // printf("%d %d %d\n", low, high, mid);
+        mid = (low + high)/2;
+        printf("%d %d %d\n", low, high, mid);
+
+        if (arr[mid] == k)
+        {
+            cout<< mid << " " << "found" << "\n";
+            return true;
+        }
+        else if (arr[mid] < k)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return false;
+}
+
+/*
+8 50
+1 2 3 4 5 6 7 8
+*/
+
